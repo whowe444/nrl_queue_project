ec4224d01cfba6f9bad5d22c09b5b7f6dab26a9d
whowe444 <willhowe235@gmail.com>
2023-12-03 12:06:57 -0500

    single return statement

--- src/queue.c
+++ src/queue.c
@@ -10,44 +10,37 @@ static node* gp_end_of_line = &g_sentinel;
 int
 enqueue(const char* a_data)
 {
+    int t_return_val = -1;
     node* t_new_node = generate_node(a_data, NULL);
-    if (!t_new_node)
+    if (t_new_node)
     {
-        return -1;
+        gp_end_of_line->next = t_new_node;
+        gp_end_of_line = t_new_node;
+        g_size++;
+        t_return_val = 0;
     }
-
-    gp_end_of_line->next = t_new_node;
-    gp_end_of_line = t_new_node;
-
-    g_size++;
-
-    return 0;
+    return t_return_val;
 }
 
 const char*
 dequeue()
 {
     const char* t_return_str = NULL;
-
-    if (size() == 0)
-    {
-        return t_return_str;
-    }
-
     node* t_return_node = g_sentinel.next;
-
-    // handle the special case of size == 1
-    if (t_return_node == gp_end_of_line)
+    if (t_return_node != NULL)
     {
-        gp_end_of_line = &g_sentinel;
+        // handle the special case of size == 1
+        if (t_return_node == gp_end_of_line)
+        {
+            gp_end_of_line = &g_sentinel;
+        }
+
+        g_sentinel.next = t_return_node->next;
+        t_return_str = t_return_node->data;
+        free(t_return_node);
+
+        g_size--;
     }
-
-    g_sentinel.next = t_return_node->next;
-    t_return_str = t_return_node->data;
-    free(t_return_node);
-
-    g_size--;
-
     return t_return_str;
 }
 

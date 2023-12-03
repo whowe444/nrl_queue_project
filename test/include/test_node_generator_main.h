bbf1f2da635c14c06c6857493a90fbee27925f8a
whowe444 <willhowe235@gmail.com>
2023-12-02 22:47:17 -0500

    checkpoint

---
+++ test/include/test_node_generator_main.h
@@ -0,0 +1,8 @@
+#ifndef TEST_NODE_GENERATOR_MAIN_H
+#define TEST_NODE_GENERATOR_MAIN_H
+
+#include "node_generator.h"
+
+void test_generate_node();
+
+#endif /* TEST_NODE_GENERATOR_MAIN_H */
---
+++ test/test_node_generator_main.c
@@ -0,0 +1,16 @@
+#include "test_node_generator_main.h"
+
+#include "assert.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+void test_generate_node()
+{
+    assert(true);
+    printf("TEST test_generate_node(): SUCCESS\n");
+}
+
+int main()
+{
+    test_generate_node();
+}
